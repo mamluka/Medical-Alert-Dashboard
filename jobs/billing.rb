5a8c235b94b5d287b2951928b90dfd012e981d9b
david mz <david.mazvosky@gmail.com>
2013-10-31 14:25:01 -0700

    update new payment idea

--- jobs/billing.rb
+++ jobs/billing.rb
@@ -15,36 +15,28 @@ SCHEDULER.every '10m' do
 
   client = Watir::Browser.new :phantomjs
 
-  client.goto "https://www.administration123.com/manage/billing/index.cfm?transaction_created_from=#{past_week_start_date.strftime('%m/%d/%Y')}&transaction_created_to=#{past_week_end_date.strftime('%m/%d/%Y')}"
+  client.goto "https://www.administration123.com/manage/users/index.cfm?activefilter=active&productSearchType=ANY&rd_product=1&rd_product_not=1&dtBillingStart=#{past_week_start_date.strftime('%m/%d/%Y')}&dtBillingEnd=#{past_week_end_date.strftime('%m/%d/%Y')}&holdSearchType=include&bAllPayments=0&reportId=681&pageSize=100"
   client.text_field(name: 'username').set 'Ellick Data Entry'
   client.text_field(name: 'password').set 'Ellick887711'
 
   client.button(name: 'authenticate').click
 
   text = client.text
-  past_week_confirmed = text.scan(/Approved:\s(\d+?)\s/)[0][0]
-  past_week_declined = text.scan(/Declined:\s(\d+?)\s/)[0][0]
+  past_week_confirmed = text.scan(/Total::\s(\d+?)\s/)[0][0]
 
   send_event('confirmed-past-week-sales', {current: past_week_confirmed, last: current_past_week_confirmed_sales})
   current_past_week_confirmed_sales = past_week_confirmed
 
-  send_event('declined-past-week-sales', {current: past_week_declined, last: current_past_week_declined_sales})
-  current_past_week_declined_sales = past_week_declined
-
   this_week_start_date = Time.new.wday == 1 ? Time.new : Chronic.parse('last monday')
   this_week_end_date = Time.new
 
-  client.goto "https://www.administration123.com/manage/billing/index.cfm?transaction_created_from=#{this_week_start_date.strftime('%m/%d/%Y')}&transaction_created_to=#{this_week_end_date.strftime('%m/%d/%Y')}"
+  client.goto "https://www.administration123.com/manage/users/index.cfm?activefilter=active&productSearchType=ANY&rd_product=1&rd_product_not=1&dtBillingStart=#{this_week_start_date.strftime('%m/%d/%Y')}&dtBillingEnd=#{this_week_end_date.strftime('%m/%d/%Y')}&holdSearchType=include&bAllPayments=0&reportId=681&pageSize=100"
 
   text = client.text
-  this_week_confirmed = text.scan(/Approved:\s(\d+?)\s/)[0][0]
-  this_week_declined = text.scan(/Declined:\s(\d+?)\s/)[0][0]
+  this_week_confirmed = text.scan(/Total::\s(\d+?)\s/)[0][0]
 
   send_event('confirmed-this-week-sales', {current: this_week_confirmed, last: current_this_week_confirmed_sales})
   current_this_week_confirmed_sales = this_week_confirmed
-
-  send_event('declined-this-week-sales', {current: this_week_declined, last: current_this_week_declined_sales})
-  current_this_week_declined_sales = this_week_declined
 
   client.close
 
